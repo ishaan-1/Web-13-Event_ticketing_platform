a9645f63207690c7d7a740a184bfbfe56219ca5b
varshneyanushka <anushka.pkg@gmail.com>
2023-07-22 18:07:06 +0530

    updated

--- etp/organizer/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Eventdetails,Ticket
-# Register your models here.
-admin.site.register(Eventdetails)
-admin.site.register(Ticket)
--- etp/user/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-from organizer.models import Eventdetails
-# Create your models here.
-class BookedTickets(models.Model):
-    email = models.CharField(max_length=100)
-    event = models.ForeignKey(Eventdetails, on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return f'{self.email} - {self.event.eventName}'
